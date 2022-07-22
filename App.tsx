d2bd2f21326bf6fe305ea228c0975a3f6779bd38
Deepak S <s.deepak120@gmail.com>
2022-07-22 11:32:45 +0530

    Authenticating user from AWS Amplify

--- App.tsx
+++ App.tsx
@@ -6,13 +6,32 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import { withAuthenticator } from 'aws-amplify-react-native';
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
+import { useEffect } from "react";
 Amplify.configure(awsconfig);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  //Run this snippet only when the app is mounted
+  useEffect( () => {
+    const fetchuser = async () => {
+      //get authenticated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      
+      if(userInfo) {
+
+      }
+      
+      //get the user from Backend with userid from Auth 
+
+
+      //if there is no user in db then create one
+    }
+    fetchuser();
+  },[])
 
   if (!isLoadingComplete) {
     return null;
